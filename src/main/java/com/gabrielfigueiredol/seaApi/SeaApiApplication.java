72753b60b0a7b837081aff7fd0471136bfa6d399
gabrielfigueiredol <gabrieellfigueiredo@gmail.com>
2025-06-17 14:19:33 -0300

    tada: first commit

---
+++ src/main/java/com/gabrielfigueiredol/seaApi/SeaApiApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielfigueiredol.seaApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielfigueiredol/seaApi/SeaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielfigueiredol.seaApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
