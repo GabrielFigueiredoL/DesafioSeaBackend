c1bc6dddbbf335de7e845f0224fdf52afc3ab1bb
gabrielfigueiredol <gabrieellfigueiredo@gmail.com>
2025-06-21 21:23:38 -0300

    feat: implement bad credentials and access denied custom error

--- src/main/java/com/gabrielfigueiredol/seaApi/infra/RestExceptionHandler.java
+++ src/main/java/com/gabrielfigueiredol/seaApi/infra/RestExceptionHandler.java
@@ -5,11 +5,14 @@ import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.nio.file.AccessDeniedException;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -26,5 +29,17 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
         ErrorResponseDTO responseDTO = new ErrorResponseDTO(errorMessage, String.format("Error(s) found: %d", errorMessages.size()), LocalDateTime.now());
         return ResponseEntity.badRequest().body(responseDTO);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ErrorResponseDTO> handleBadCredentials(BadCredentialsException ex) {
+        ErrorResponseDTO responseDTO = new ErrorResponseDTO("Credenciais inválidas", "Login ou senha incorretos", LocalDateTime.now());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseDTO);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorResponseDTO> handleAccessDeniedException (AccessDeniedException ex) {
+        ErrorResponseDTO responseDTO = new ErrorResponseDTO("Acesso negado", "Credenciais de acesso negadas", LocalDateTime.now());
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(responseDTO);
     }
 }
